d53cfbc9a29193849448488331ca063b0557e542
yuanguozhu157-cpu <yuanguozhu157@gmail.com>
2025-09-20 12:08:35 +0800

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,2 @@
+import '@/styles/globals.css'
+export default function MyApp({Component,pageProps}){return <Component {...pageProps}/>}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold text-blue-600">欢迎来到 1 Phi Saying 一对一教学网站</h1>
+      <p className="mt-4 text-lg">这是一个测试首页，说明网站部署成功 🚀</p>
+    </Layout>
+  )
+}
